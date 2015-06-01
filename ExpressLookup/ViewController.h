150f409884c1f0a61bfdfe73db8589db95543e16
KyonLi <i@kyonli.com>
2015-06-01 10:49:37 +0800

    Initial Commit

---
+++ ExpressLookup/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExpressLookup
+//
+//  Created by Kyon on 15/6/1.
+//  Copyright (c) 2015年 Kyon Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ExpressLookupTests/ExpressLookupTests.m
@@ -0,0 +1,40 @@
+//
+//  ExpressLookupTests.m
+//  ExpressLookupTests
+//
+//  Created by Kyon on 15/6/1.
+//  Copyright (c) 2015年 Kyon Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ExpressLookupTests : XCTestCase
+
+@end
+
+@implementation ExpressLookupTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
